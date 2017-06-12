84333b0f06849bb2f09840249f88b28316773054
Masaya Takeda <844196@gmail.com>
2017-06-12 14:39:59 +0900

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# franz-chatwork
+
+franz-chatwork provide [Chatwork](http://www.chatwork.com/ja/) tab on [Franz](http://meetfranz.com/)
+
+## VS default
+
+* Badge (reply and unread in a channel)
+
+## Installation
+
+```console
+% git clone https://github.com/844196/franz-chatwork <DESTINATION_PATH>
+```
+
+### Destination path
+
+* Linux
+    * `~/.config/Franz/Plugins/chatwork`
+* Mac
+    * `~/Library/Application\ Support/Franz/Plugins/chatwork`
+* Windows
+    * `%APPDATA%\Franz\Plugins\chatwork`
+
+## License
+
+MIT
+
+## Author
+
+Masaya Tk (<https://github.com/844196>)
---
+++ webview.js
@@ -0,0 +1,24 @@
+module.exports = (Franz, options) => {
+  const polling = () => {
+    let target = {
+      unread: {
+        count: 0,
+        element: document.getElementById('_chatUnreadStatus'),
+      },
+      reply: {
+        count: 0,
+        element: document.getElementById('_chatToUnreadStatus'),
+      },
+    };
+
+    Object.keys(target).forEach((type) => {
+      if (target[type].element.style.display != 'none') {
+        target[type].count = parseInt(target[type].element.innerText);
+      }
+    });
+
+    Franz.setBadge(target.reply.count, target.unread.count);
+  };
+
+  Franz.loop(polling);
+}
